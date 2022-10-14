beee0d9b0d289c0f93791c095b8625ef19db744b
Maria Yutaeva <mariya1982@yandex.ru>
2022-10-14 13:08:28 +0300

    задача63

---
+++ Task63/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 63: Задайте значение вещественное N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.WriteLine("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+FindNatural(number);
+void FindNatural(int number, int count = 0)
+{
+    count++;
+    Console.Write(count + " ");
+    if (count >= number)
+    {
+        return;
+    }
+    FindNatural(number, count);
+}
