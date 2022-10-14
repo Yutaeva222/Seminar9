da1c300f016e400ba1997ac7dbffaf378de7c4d3
Maria Yutaeva <mariya1982@yandex.ru>
2022-10-14 13:38:16 +0300

    задача65

---
+++ Task65/Program.cs
@@ -0,0 +1,20 @@
+﻿ // Задача 65. Задайте значения M и N. Напишите программу, которая выведет все натуральные числа
+ // в промежутке этих чисел.
+
+  
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+FindNatural(N,M);
+void FindNatural(int N, int count = 1)
+{
+   
+    Console.Write(count + " ");
+    count++;
+    if (count >= N)
+    {
+        return;
+    }
+    FindNatural(N, count);
+}
